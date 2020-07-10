f6bb9c202955d29c192620e8648e9516a045827f
Andrey Saprykin <asaprykin@griddynamics.com>
2020-07-10 16:45:11 +0400

    Add: window with watermark

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,7 @@
+import java.sql.Timestamp
+
 import model.CaseClasses.Events
 import utils._
-
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.{OutputMode, ProcessingTime}
@@ -20,7 +21,6 @@ object main {
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "user-click-data")
-      .option("startingOffsets", "earliest")
       .load()
 
     val valueDs = dfStream.selectExpr("CAST(value AS STRING) as value")
@@ -30,10 +30,14 @@ object main {
             case str: String => parseString(str)
             case _ => Nil
           }
-      }.as[List[String]].filter(_.nonEmpty).map(a => Events(a(0).toLong, a(1).toInt, a(2), a(3)))
+      }.as[List[String]].filter(_.nonEmpty).map(a => Events(new Timestamp(a(0).toLong * 1000), a(1).toInt, a(2), a(3)))
+      .filter(_.ip == "172.20.0.0")
 
-    val intermediateDf = valueDs.writeStream
-      .outputMode(OutputMode.Update())
+    val intermediateDf = valueDs.withWatermark("unix_time", "20 seconds")
+      .groupBy(window($"unix_time", "10 seconds", "10 seconds"), $"ip")
+      .count()
+      .writeStream
+      .outputMode(OutputMode.Append())
       .option("truncate", false)
       .trigger(ProcessingTime("5 seconds"))
       .format("console").start()
--- src/main/scala/model/CaseClasses.scala
+++ src/main/scala/model/CaseClasses.scala
@@ -1,5 +1,7 @@
 package model
 
+import java.sql.Timestamp
+
 object CaseClasses {
-  case class Events(unix_time: Long, category_id: Int, ip: String, `type`: String)
+  case class Events(unix_time: Timestamp, category_id: Int, ip: String, `type`: String)
 }

9d8184b18dd84fdfb4f37bb1c6da04f5cf9f3d31
Andrey Saprykin <asaprykin@griddynamics.com>
2020-07-14 16:15:09 +0400

    Add: Cassandra support [DStream]

--- src/main/scala/dstream.scala
+++ src/main/scala/dstream.scala
@@ -1,5 +1,6 @@
 import java.sql.Timestamp
 
+import com.datastax.spark.connector
 import com.redis.RedisClient
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
@@ -7,6 +8,10 @@ import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.{Seconds, State, StateSpec, StreamingContext}
+import com.datastax.spark.connector._
+import model.CaseClasses.Events
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import sstream.localhost
 
 import scala.collection.mutable.Map
 import utils._
@@ -18,9 +23,20 @@ object dstream {
 
   def main(args: Array[String]): Unit = {
 
-    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("NetworkWordCount")
+
     val ssc = new StreamingContext(conf, Seconds(5))
     ssc.checkpoint("/Users/asaprykin/Documents/lpProjects/In-stream-assignment-project/file/checkpoint-location/dstream")
+
+    val sparkSession = SparkSession.builder()
+      .config(ssc.sparkContext.getConf)
+      .config("spark.cassandra.connection.host", localhost)
+      .withExtensions(new CassandraSparkExtensions)
+      .config("spark.sql.catalog.mycatalog", "com.datastax.spark.connector.datasource.CassandraCatalog")
+      .config("spark.cassandra.output.consistency.level", "ONE")
+      .getOrCreate()
 
     val kafkaParam = Map[String, Object](
       "bootstrap.servers" -> "127.0.0.1:9092",
@@ -38,11 +54,11 @@ object dstream {
     )
 
     def accumulateMappingFunction (key: (Timestamp, Timestamp, String),
-                                   value: Option[Int],
-                                   state: State[Int]): ((Timestamp, Timestamp, String), Int)  = {
+                                   value: Option[(Events, Int)],
+                                   state: State[Int]): ((Timestamp, Timestamp, String), (Events, Int))  = {
       value match {
-        case Some(x) => state.update(state.getOption().getOrElse(0) + x); (key, state.get)
-        case None => (key, 0)
+        case Some(x) => state.update(state.getOption().getOrElse(0) + x._2); (key, (x._1, state.get))
+        case None => (key, (Events(new Timestamp(0), 0, "", ""), 0))
       }
     }
 
@@ -50,21 +66,22 @@ object dstream {
       .map(_.value)
       .map(x => parseString(x))
       .filter(_.nonEmpty)
-      .map(a => (createTriple(a(0).toDouble, a(2)), 1))
+      .map(a =>
+        (createTriple(a(0).toDouble, a(2)),
+          (Events(new Timestamp(a(0).toLong * 1000), a(1).toInt, a(2), a(3)), 1)))
       .mapWithState(StateSpec.function(accumulateMappingFunction _))
       .foreachRDD(rdd => {
-        rdd.reduceByKey((x, y) => if (x > y) x else y)
+
+        rdd.reduceByKey((x, y) => if (x._2 > y._2) x else y)
           .collect().foreach( value => {
             val ip = value._1._3
-            val requests = value._2
+            val requests = value._2._2
             val redisKey = "dbots:"+ip
 
             if (requests > 20) {
-              println(ip + "      " + requests)
               redis.hset(redisKey, "requests", requests)
               redis.hset(redisKey, "added_time", System.currentTimeMillis()/1000)
             } else {
-              println(ip + "      " + requests)
               val added_time_optional = redis.hget(redisKey, "added_time")
               added_time_optional.map(added_time => {
                 if (System.currentTimeMillis()/1000 - added_time.toLong > 600)
@@ -72,6 +89,18 @@ object dstream {
               })
             }
           })
+
+        sparkSession.createDataFrame(rdd.map(_._2._1).collect().map(event => {
+          val optionBot = redis.hget("dbots:" + event.ip, "added_time")
+          (event.ip, event.category_id, event.unix_time, event.`type`,
+            if (optionBot.isDefined) true else false)
+        })).toDF("ip", "category_id", "unix_time", "type", "is_bot").write
+          .format("org.apache.spark.sql.cassandra")
+          .option("keyspace", "event_click")
+          .option("table", "events_ds")
+          .mode("APPEND")
+          .save()
+
       })
 
     ssc.start

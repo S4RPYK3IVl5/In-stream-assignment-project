3731ef034b17d5bec76ca32138a0456995455736
Andrey Saprykin <asaprykin@griddynamics.com>
2020-07-14 09:58:39 +0400

    Add: Finish part one

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,14 +1,13 @@
 import java.sql.Timestamp
 
-import com.datastax.oss.protocol.internal.ProtocolConstants.ConsistencyLevel
 import com.datastax.spark.connector.CassandraSparkExtensions
 import com.redis.RedisClient
 import model.CaseClasses.Events
-import utils._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.{OutputMode, ProcessingTime}
 import org.apache.spark.sql.types._
+import utils._
 
 object main {
 
@@ -74,15 +73,78 @@ object main {
       .add("ip", "string")
       .add("count", "integer")
     val readScvFiles = sparkSession
-        .readStream
-        .option("sep", ",")
-        .schema(eventCSVSchema)
-        .csv(pathToCSTFilesIntermediate)
+      .readStream
+      .option("sep", ",")
+      .schema(eventCSVSchema)
+      .csv(pathToCSTFilesIntermediate)
 
     val ipWithIndicator = readScvFiles.groupBy($"ip")
       .agg(sum($"count").as("event_sum"), count($"ip").as("count_of_window"))
       .withColumn("indicator", $"event_sum" / $"count_of_window")
 
+//    val iipWithIndicator = ipWithIndicator.withColumnRenamed("ip", "iip")
+//        .withColumn("water_time", current_timestamp())
+//        .withWatermark("water_time", "20 seconds")
+//
+//    // Writing to Cassandra // DOES NOT WORK, BUT HAVE TO BE PRESENTED
+//    valueDs
+//      .withWatermark("unix_time", "20 seconds")
+//      .join(iipWithIndicator, valueDs("ip") === iipWithIndicator("iip"), "inner")
+////      .join(iipWithIndicator, expr(
+////        s"""
+////          |ip = iip AND
+////          |water_time + interval 20 seconds > ${current_timestamp()} AND
+////          |unix_time + interval 20 seconds > ${current_timestamp()}
+////          |""".stripMargin), "inner")
+//      .withColumn("is_bot", $"event_sum" >= 20.0)
+//      .select($"ip", $"category_id", $"unix_time", $"type", $"is_bot")
+//      .writeStream
+//      .outputMode(OutputMode.Append)
+//      .trigger(ProcessingTime("5 seconds"))
+//      .foreachBatch((ds, _) =>
+//        ds.write
+//          .format("org.apache.spark.sql.cassandra")
+//          .option("keyspace", "event_click")
+//          .option("table", "events")
+//          .mode("APPEND")
+//          .save()
+//        ).start()
+
+    // Writing to Cassandra //
+    valueDs.writeStream
+      .outputMode(OutputMode.Update)
+      .trigger(ProcessingTime("5 seconds"))
+      .foreachBatch((ds, _) => {
+
+        val cachedBots = sparkSession.read
+          .format("org.apache.spark.sql.redis")
+          .schema(
+            StructType(Array(
+              StructField("ip", StringType),
+              StructField("event_sum", IntegerType),
+              StructField("indicator", DoubleType),
+              StructField("count_of_window", IntegerType),
+              StructField("added_time", LongType))
+            )
+          )
+          .option("keys.pattern", "bots:*")
+          .option("key.column", "ip")
+          .load().toDF("rIp", "rEvent_sum", "rIndicator", "rCount_of_window", "rAdded_time")
+
+        ds.join(cachedBots, ds("ip") === cachedBots("rIp"), "left")
+          .withColumn("is_bot", $"rIp".isNotNull)
+          .select($"ip", $"category_id", $"unix_time", $"type", $"is_bot")
+          .write
+          .format("org.apache.spark.sql.cassandra")
+          .option("keyspace", "event_click")
+          .option("table", "events")
+          .mode("APPEND")
+          .save()
+
+      }).start
+    // //
+
+    // Writing to Redis //
     ipWithIndicator.writeStream
       .outputMode(OutputMode.Update)
       .trigger(ProcessingTime("5 seconds"))
@@ -99,11 +161,11 @@ object main {
               StructField("added_time", LongType))
             )
           )
-          .option("keys.pattern", "bot:*")
+          .option("keys.pattern", "bots:*")
           .option("key.column", "ip")
           .load().toDF("rIp", "rEvent_sum", "rIndicator", "rCount_of_window", "rAdded_time")
 
-        val botsDf = ds.filter($"event_sum" >= 20.0)
+        val botsDf = ds.filter($"indicator" >= 20.0)
           .withColumn("added_time", current_timestamp().cast(LongType))
 
         val joinedDf = cachedBots.join(botsDf, $"rIp" === $"ip", "left")
@@ -111,25 +173,17 @@ object main {
         val whiteListedDf = joinedDf
           .filter($"ip".isNull && (current_timestamp().cast(LongType) - $"rAdded_time") > 600)
         if(!whiteListedDf.isEmpty) whiteListedDf
-          .foreach(row => redis.del(s"bot:${row.getAs[String]("rIp")}"))
+          .foreach(row => redis.del(s"bots:${row.getAs[String]("rIp")}"))
 
         botsDf.write
           .format("org.apache.spark.sql.redis")
-          .option("table", "bot")
+          .option("table", "bots")
           .option("key.column", "ip")
           .mode(SaveMode.Append)
           .save()
 
-        ds.withColumn("is_bot", $"event_sum" >= 20.0)
-          .withColumn("added_time", current_timestamp().cast(LongType))
-          .write
-          .format("org.apache.spark.sql.cassandra")
-          .option("keyspace", "event_click")
-          .option("table", "event")
-          .mode("APPEND")
-          .save()
-
       }).start
+    // //
 
     sparkSession.streams.awaitAnyTermination()
 
